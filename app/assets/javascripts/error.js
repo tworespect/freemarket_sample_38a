87c198e055267bb8a9a6b221a76fe1552f85640a
紀　祐司 <signtheswish@gmail.com>
2018-11-14 13:23:31 +0900

    add_js

---
+++ app/assets/javascripts/error.js
@@ -0,0 +1,27 @@
+$(function() {
+
+  var list =  $('li').text();
+
+  if(list.includes("Name can't be blank")){
+      $('.name-error').css('display','inherit');
+  }
+  if(list.includes("Email can't be blank")){
+      $('.email-error').css('display','inherit');
+  }
+  if(list.includes("Password can't be blank")){
+        $('.password-error').css('display','inherit');
+        $('.password-error-message').css('display','inherit');
+  }
+  if(list.includes("Password is too short (minimum is 6 characters)")){
+        $('.password-error-message').css('display','inherit');
+  }
+  if(list.includes("Password confirmation can't be blank")){
+      $('.password_confirmation-error').css('display','inherit');
+  }
+  if(list.includes("Password confirmation doesn't match Password")){
+      $('.no-match-error').css('display','inherit');
+  }
+  if(list.includes("Email has already been taken")){
+      $('.already-taken').css('display','inherit');
+  }
+});
