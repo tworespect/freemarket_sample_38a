d83969247055e91c658a1fe90679bd55e03ad4d4
紀　祐司 <signtheswish@gmail.com>
2018-11-21 11:22:10 +0900

    Adjust_view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,7 @@ class ProductsController < ApplicationController
   def show
   end
 
+  def buy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
+  get "products/buy", to: "products#buy"
   resources :products
   get "users/logout", to: "users#logout"
   resources :users, only: [:index, :show, :edit, :update, :new]
