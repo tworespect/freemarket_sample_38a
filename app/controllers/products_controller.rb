3fc598c9f076dbcd4a5f00d05780027b81d9e78b
izuharayuichi <y12i26r53@hotmail.co.jp>
2018-11-24 13:29:41 +0900

    edit_exhibit_page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,14 +9,22 @@ class ProductsController < ApplicationController
   end
 
   def create
+    binding.pry
     @product  = Product.new(product_params)
-    @product.save!
-    redirect_to ''
+    # if @product.save
+    #   respond_to do |format|
+    #     format.js
+    #   end
+    # else
+    #   render :new
+    # end
+    @product.save
+    render :new
   end
 
   def buy
   end
-  
+
 
   private
 
