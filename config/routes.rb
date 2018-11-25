6ea9c855da3bcd995446e44e917f9ba56f39c9cb
daisuke matsumoto <daisuke.matsumoto@daisukenoMacBook-Air.local>
2018-11-25 18:42:25 +0900

    Credit決済、カード情報入力・保存に関するルーティングを記述
    Credit決済、カード情報入力・保存に関するルーティングを記述

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   root 'products#index'
   resources :products
   get "users/logout", to: "users#logout"
-  resources :users, only: [:index, :show, :edit, :update, :new]
-
+  resources :users, only: [:index, :show, :edit, :update, :new] do
+    resources :card, only: [:index, :new, :create]
+  end
+  root 'charges#index'
+  get 'charges' => 'charges#index'
+  post 'charges/pay' => 'charges#pay'
+  get 'charges/pay' => 'charges#pay'
 
 end
