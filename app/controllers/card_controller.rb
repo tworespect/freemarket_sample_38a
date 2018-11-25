2a9bd138e46c810f2a157b88c2a42b68e2e5c1d7
daisuke matsumoto <daisuke.matsumoto@daisukenoMacBook-Air.local>
2018-11-25 18:34:46 +0900

    'Credit情報を作成・保存・表示'に関するコントローラ作成
    'Credit情報を作成・保存・表示'に関するコントローラを作成、実装

---
+++ app/controllers/card_controller.rb
@@ -0,0 +1,99 @@
+class CardController < ApplicationController
+
+  def index
+    @mycard = Payjp::Customer.retrieve(current_user.payments.first.payjp_customer_id).cards.data[0] if current_user.payments.present?
+  end
+
+  def new
+
+  end
+
+  def create
+    unless current_user.payments.present?
+      Payjp.api_key = PAYJP_SECRET_KEY
+
+      year = "20" + params[:exp_year]
+      token = Payjp::Token.create({
+        card: {
+          number: params[:number],
+          cvc: params[:cvc],
+          exp_month: params[:exp_month],
+          exp_year: year,
+          name: params[:name]
+        }},
+        {
+          'X-Payjp-Direct-Token-Generate': 'true'
+        }
+      )
+      customer_id = Payjp::Customer.create(card: token.id).id
+      @payment = Payment.find_or_initialize_by(user_id: current_user.id)
+
+      @payment.payjp_customer_id = customer_id
+
+      if @payment.save
+        render :index
+      else
+        render :new
+      end
+    else
+      render :index
+    end
+  end
+
+  private
+  def payment_params
+    params.permit(:payment).permit(:user_id, :customer_id)
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# console
+
+# pay.jP ni access
+#  Payjp.api_key ireru
+# costomer create
+# Payjp::Customer.create
+# user miru
+# Payjp::Customer.all
+# take user
+# Payjp::Customer.retirve('user id')
+
+
+# goal
+
+# creaditcard show
+
+# 1 find user
+# 2 fid card   user payment id   Payjp::Customer.retirve('user id').cards
+
+
+# Payment id ni  pay.jp user id
+
+
+# # assosication
+# # rails console
+
+# model.find(:id)
+
+
+
+
+
+
+
+
+
