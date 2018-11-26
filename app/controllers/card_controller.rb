a28ab7676dfb47e784f4e7c7ba30b17d9446ae3c
daisuke matsumoto <daisuke.matsumoto@daisukenoMacBook-Air.local>
2018-11-26 15:19:07 +0900

    cardコントローラのアクションをmoduleに移動
    Cardコントローラのアクションをmoduleに移動
    メソッド化

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,5 +1,7 @@
 class CardController < ApplicationController
 
+include Card
+
   def index
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     @mycard = Payjp::Customer.retrieve(current_user.payments.first.payjp_customer_id).cards.data[0] if current_user.payments.present?
@@ -9,37 +11,6 @@ class CardController < ApplicationController
 
   end
 
-  def create
-    unless current_user.payments.present?
-      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
-
-      year = "20" + params[:exp_year]
-      token = Payjp::Token.create({
-        card: {
-          number: params[:number],
-          cvc: params[:cvc],
-          exp_month: params[:exp_month],
-          exp_year: year,
-          name: params[:name]
-        }},
-        {
-          'X-Payjp-Direct-Token-Generate': 'true'
-        }
-      )
-      customer_id = Payjp::Customer.create(card: token.id).id
-      @payment = Payment.find_or_initialize_by(user_id: current_user.id)
-
-      @payment.payjp_customer_id = customer_id
-
-      if @payment.save
-        render :index
-      else
-        render :new
-      end
-    else
-      render :index
-    end
-  end
 
   private
   def payment_params
