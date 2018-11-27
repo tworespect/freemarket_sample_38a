5d162d0314750694cb3901e35e8143c1daeba6de
紀　祐司 <signtheswish@gmail.com>
2018-11-27 11:38:51 +0900

    edit_deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,14 +33,14 @@ namespace :deploy do
   end
 
   # desc 'upload secrets.yml'
-  # task :upload do
-  #   on roles(:app) do |host|
-  #     if test "[ ! -d #{shared_path}/config ]"
-  #       execute "mkdir -p #{shared_path}/config"
-  #     end
-  #     upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
-  #   end
-  # end
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      # upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
   before :starting, 'deploy:upload'
   after :finishing, 'deploy:cleanup'
 end
