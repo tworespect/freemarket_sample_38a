e72da0b3ea3149fb72bc45efb8e155b29ba57acf
izuharayuichi <y12i26r53@hotmail.co.jp>
2018-11-28 19:11:31 +0900

    Products/show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,6 +21,10 @@ class ProductsController < ApplicationController
     else
       redirect_to new_product_path
     end
+
+  def show
+    @pre_product = Product.order("RAND()").limit(1)
+    @post_product = Product.order("RAND()").limit(1)
   end
 
   def edit
@@ -36,7 +40,6 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    binding.pry
     if @product.destroy
       redirect_to products_path
     else
@@ -50,7 +53,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :price, :freight, :state_of_goods, :description, :ship_method, :ship_from_location, :ship_day, product_sizes_attributes:[:id, :size_id], product_brands_attributes:[], product_categories_attributes:[:id, :category_id], images_attributes:[:id, :first_image, :second_image, :third_image, :forth_image]).merge(status: 0, user_id: current_user.id)
+    params.require(:product).permit(:name, :price, :freight, :state_of_goods, :description, :ship_method, :ship_from_location, :ship_day, product_sizes_attributes:[:id, :size_id], product_categories_attributes:[:id, :category_id], images_attributes:[:id, :first_image, :second_image, :third_image, :forth_image]).merge(product_brands_attributes:[1], status: 0, user_id: current_user.id)
   end
 
   def set_product
