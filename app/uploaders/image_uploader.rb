bc209f212a0068c095746db3d7176413826b6576
紀　祐司 <signtheswish@gmail.com>
2018-11-28 15:19:22 +0900

    edit_uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,7 +6,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   if Rails.env.development?
     storage :file
-  else Rail.env.product?
+  elsif Rails.env.production?
     storage :fog
   end
 
