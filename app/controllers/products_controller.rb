86ebfaaa09e7782650ef4b60a3ec875c76136df6
izuharayuichi <y12i26r53@hotmail.co.jp>
2018-11-28 21:09:59 +0900

    Error500 解消

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,11 +16,13 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    if @product.save
-      redirect_to products_path
-    else
-      redirect_to new_product_path
-    end
+    @product.save
+    redirect_to products_path
+    # if @product.save
+    #   redirect_to products_path
+    # else
+    #   redirect_to new_product_path
+    # end
   end
 
   def show
@@ -54,7 +56,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :price, :freight, :state_of_goods, :description, :ship_method, :ship_from_location, :ship_day, product_sizes_attributes:[:id, :size_id], product_categories_attributes:[:id, :category_id], images_attributes:[:id, :first_image, :second_image, :third_image, :forth_image]).merge(product_brands_attributes:[1], status: 0, user_id: current_user.id)
+    params.require(:product).permit(:name, :price, :freight, :state_of_goods, :description, :ship_method, :ship_from_location, :ship_day, product_sizes_attributes:[:id, :size_id], product_brands_attributes:[], product_categories_attributes:[:id, :category_id], images_attributes:[:id, :first_image, :second_image, :third_image, :forth_image]).merge(product_brands_attributes:[], status: 0, user_id: current_user.id)
   end
 
   def set_product
