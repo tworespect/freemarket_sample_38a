3a5b62e9ae919ba75a589637fb949742149d6ae5
izuharayuichi <y12i26r53@hotmail.co.jp>
2018-11-29 20:16:16 +0900

    ブランドid取得（インクリメンタルサーチ）

--- app/assets/javascripts/channels/exhibit_page/exhibit_page_brand.js
+++ app/assets/javascripts/channels/exhibit_page/exhibit_page_brand.js
@@ -1,35 +1,41 @@
 $(function(){
 
+  var input    = $(".medium_box__text").val();
+  var search   = $('.block--product_detail--right__brand__candidate');
+  var register = $('.block--product_detail--right__brand');
+
+
   function appendName(brand){
-    var html = `<a class="brand-search-add${ brand.id }">
-                  <div class="block--product_detail--right__brand__candidate__list">
-                    <p>${ brand.name }</p>
-                  </div>
-                </a>`
-    $('.block--product_detail--right__brand__candidate').append(html);
+    var html = `<div class="block--product_detail--right__brand__candidate__list" id="candidate${ brand.id }">
+                  <a class="brand-search-add${ brand.id } block--product_detail--right__brand__candidate__list__each" data-brand-id="${ brand.id }">
+                    ${ brand.name }
+                  </a>
+                </div>`
+    search.append(html);
   }
 
   function appendNoName(text){
-    var html = `<a class="brand-search-add11">
-                  <div class="block--product_detail--right__brand__candidate__list">
-                    <p>${ text }</p>
-                  </div>
-                </a>`
-    $('.block--product_detail--right__brand__candidate').append(html);
+    var html = `<div class="block--product_detail--right__brand__candidate__list">
+                  <a class="brand-search-add block--product_detail--right__brand__candidate__list__each">
+                    ${ text }
+                  </a>
+                </div>`
+    search.append(html);
   }
 
   function registerName(brand){
-    var html = `<div class="medium_box--brand">
-                  <% if user_signed_in? %>
-                    <div>a</div>
-                  <% end %>
-                </div>`
-    $('.block--product_detail--right__brand').append(html);
+    var brand_name = $(`.brand-search-add${ brand.id }`).text()
+    var brand_id   = $(`.brand-search-add${ brand.id }`).attr('data-brand-id')
+
+    $('.medium_box__text').val('');
+    $('.medium_box__text').val( brand_name )
+    $('.medium_box--brand__input').val( brand_id )
+    $('.block--product_detail--right__brand__candidate').css('display','none');
   }
 
 
-
-  $(".medium_box__text").on('keyup', function(e){
+  $(".medium_box__text").on('keyup', function(brand){
+    $('.medium_box--brand__input').val('')
     var input = $(".medium_box__text").val();
 
     $.ajax({
@@ -41,25 +47,26 @@ $(function(){
 
     .done(function(brands){
       $('.block--product_detail--right__brand__candidate').css('display','inherit');
-      $('.block--product_detail--right__brand__candidate__list').remove();
+      $(`.block--product_detail--right__brand__candidate__list`).remove();
       if ( brands.length !== 0 ) {
         brands.forEach(function(brand){
           appendName(brand);
-          $(`.brand-search-add${ brand.id }`).on('mouseover', function(){
-            $(`.brand-search-add${ brand.id }`).css('font-weight','600px')
+          $(`#candidate${ brand.id }`).on('mouseover', function(){
+            $(`.brand-search-add${ brand.id }`).css('font-weight','bold').css('color','white')
+            $(`#candidate${ brand.id }`).css('background-color','#0099e8')
+            $(`#candidate${ brand.id }`).on('mouseout', function(){
+              $(`.brand-search-add${ brand.id }`).css('font-weight','normal').css('color','black')
+              $(`#candidate${ brand.id }`).css('background-color','white')
+            })
           })
-          $(`.brand-search-add${ brand.id }`).on('click', function(){
-            $('.medium_box--brand, .block--product_detail--right__brand__candidate').remove();
+          $(`#candidate${ brand.id }`).on('click', function(){
             registerName(brand)
-            // $('.medium_box__text').val(${ brand.name });
-            // $('.medium_box__text').val().replace('${ brand.id }', '${ brand.id }')
           })
         })
       } else {
-        appendNoName("その他");
+        appendNoName("一致するブランドはありません");
       }
     })
-
     .fail(function() {
       alert('ブランド検索に失敗しました');
     })
