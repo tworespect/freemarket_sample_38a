71dfa513549126a2f9d41970e1c29edbc1320ed7
izuharayuichi <y12i26r53@hotmail.co.jp>
2018-11-30 02:02:03 +0900

    edit_product/Params.permit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,13 +16,12 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    @product.save
-    redirect_to products_path
-    # if @product.save
-    #   redirect_to products_path
-    # else
-    #   redirect_to new_product_path
-    # end
+    if @product.save
+      redirect_to products_path
+    else
+      @products = @product
+      render :new
+    end
   end
 
   def show
@@ -56,7 +55,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :price, :freight, :state_of_goods, :description, :ship_method, :ship_from_location, :ship_day, product_sizes_attributes:[:id, :size_id], product_brands_attributes:[], product_categories_attributes:[:id, :category_id], images_attributes:[:id, :first_image, :second_image, :third_image, :forth_image]).merge(product_brands_attributes:[], status: 0, user_id: current_user.id)
+    params.require(:product).permit(:name, :price, :freight, :state_of_goods, :description, :ship_method, :ship_from_location, :ship_day, product_sizes_attributes:[:id, :size_id], product_brands_attributes:[:id, :brand_id], product_categories_attributes:[:id, :category_id], images_attributes:[:id, :first_image, :second_image, :third_image, :forth_image]).merge(status: 0, user_id: current_user.id)
   end
 
   def set_product
